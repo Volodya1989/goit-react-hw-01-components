6b1706bbb5f736e7ed521de04c5fad7ecc34c1b2
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-05-26 19:19:09 -0400

    css added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,35 @@
 import React from "react";
+import css from "./Profile.module.css";
 
 export default function Profile(props) {
   const { username, tag, location, avatar, stats } = props;
 
   return (
-    <div className="profile">
+    <div className={css.profile}>
       <div className="description">
-        <img src={`${avatar}`} alt="User avatar" className="avatar" />
+        <img
+          src={`${avatar}`}
+          alt="User avatar"
+          className="avatar"
+          width="100px"
+        />
         <p className="name">{username}</p>
         <p className="tag">{tag}</p>
         <p className="location">{location}</p>
       </div>
-      <ul className="stats">
-        <li>
+      <ul className={css.stats}>
+        <li className={css.item}>
           <span className="label">Followers</span>
+          <br />
           <span className="quantity">{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span className="label">Views</span>
+          <br />
           <span className="quantity">{stats.views}</span>
         </li>
-        <li>
-          <span className="label">Likes</span>
+        <li className={css.item}>
+          <span className="label">Likes</span> <br />
           <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
